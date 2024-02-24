d3b25b09f6edbdbded8849ebbb5eef420d3916d6
Marton <marton.munkacsi09@gmail.com>
2024-02-24 00:29:40 +0000

    created custom User model

---
+++ backend/users/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + ((None, {"fields": ["profile_picture"]}),)
+
+
+# Register your models here.
+admin.site.register(User, CustomUserAdmin)
